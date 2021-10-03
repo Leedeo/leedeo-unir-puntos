c8efb43beef4738261a95be25a3407c241e87318
Javier Ocampos <xavier.ocampos@gmail.com>
2021-10-03 13:26:45 -0500

    Corrección de avertencias
    La explicación técnica es que cuando se crea un nuevo punto, también crea un nuevo elemento primario y recordemos que en Godot solo puede haber un elemento primario por lo que antes de crear el nuevo punto eliminamos su elemento primario para mantener una correcta estructura.

--- Draw.gd
+++ Draw.gd
@@ -16,6 +16,12 @@ func _input(event): # Usando la función nativa Input vamos a comprobar cuál es
 		if GLOBAL.pressed: # Y mantenemos un botón presionado.
 			draw_line.add_to_group("Draw") # Añadimos la línea al grupo Draw
 			draw_line.add_point(event.position) # Añadimos un punto a nuestra línea en la posición del evento, ósea donde se encuentre el mouse.
+			
+			# El suscriptor Bast Hernández tuvo la iniciativa de solucionar los avisos que genera Godot al momento de crear un nuevo punto en pantalla, me gustaría que tomasen ejemplo. Para ello fue a la documentación de add_child https://docs.godotengine.org/en/stable/classes/class_node.html hasta la parte donde dice: void add_child ( Node node, bool legible_unique_name=false ) y de ahí tomamos este condicional.
+			if draw_line.get_parent():
+				draw_line.get_parent().remove_child(draw_line)
+			# La explicación técnica es que cuando se crea un nuevo punto, también crea un nuevo elemento primario y recordemos que en Godot solo puede haber un elemento primario por lo que antes de crear el nuevo punto eliminamos su elemento primario para mantener una correcta estructura.
+			
 			add_child(draw_line) # Y con add_child añadimos el nodo a la escena.
 		else:
 			if get_tree().get_nodes_in_group("Draw"):
--- Point.gd
+++ Point.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-func _process(delta):
+func _process(_delta):
 	if not GLOBAL.pressed: get_node("Sprite").modulate = Color(1, 1, 1, 1)
 	
 
-func _on_Point_input_event(viewport, event, shape_idx):
+func _on_Point_input_event(_viewport, event, _shape_idx):
 	if GLOBAL.pressed:
 		if event is InputEventMouseMotion:
 			get_node("Sprite").modulate = Color(1, 0, 0, 1)
